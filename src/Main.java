a7da99e15b1da28a998d4728e12979d2dfef5393
lucifer10001 <aniketsolanki1001@gmail.com>
2025-05-07 21:47:55 +0530

    Solution_for_151

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import solution_with_question.Solution_151;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution_151.reverseWords("the sky is blue"));
+
+
+    }
+}
